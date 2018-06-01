0e70b3020c6062105a7e26996bfc301f15a04bfc
Benjamin Toornstra <benjamin.toornstra@vanoord.com>
2018-06-01 21:05:12 +0200

    bmrks: Added Bookmark.name property

--- docx/oxml/bookmark.py
+++ docx/oxml/bookmark.py
@@ -17,6 +17,36 @@ class CT_Bookmark(BaseOxmlElement):
     id = RequiredAttribute('w:id', ST_RelationshipId)
     name = RequiredAttribute('w:name', ST_String)
 
+    @property
+    def _next_id(self):
+        """
+        The first ``w:id`` unused by a ``<w:bookmarkStart>`` element, starting at
+        1 and filling any gaps in numbering between existing ``<w:bookmarkStart>``
+        elements.
+        """
+        bmrk_id_strs = self.xpath('.//w:bookmarkStart/@w:id')
+        bmrk_ids = [int(bmrk_id_str) for bmrk_id_str in bmrk_id_strs]
+        for num in range(1, len(bmrk_ids)+2):
+            if num not in bmrk_ids:
+                break
+        return str(num)
+
+    @property
+    def is_closed(self):
+        """
+        The `is_closed` property of the :class:`CT_BookmarkRange` object is
+        used to determine whether there is already a bookmarkEnd element in
+        the document containing the same bookmark id. If this is the case, the
+        bookmark is closed if not, the bookmark is open.
+        """
+        root_element = [ancestor for ancestor in self.iterancestors()][-1]
+        matching_bookmarkEnds = root_element.xpath(
+            './/w:bookmarkEnd[@w:id=\'%s\']' % self.id
+        )
+        if not matching_bookmarkEnds:
+            return False
+        return True
+
 class CT_MarkupRange(BaseOxmlElement):
     """The ``<w:bookmarkEnd>`` element."""
     id = RequiredAttribute('w:id', ST_RelationshipId)
