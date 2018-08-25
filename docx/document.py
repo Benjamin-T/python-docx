80065539cc76237bb845b1207ed4b4666868058d
Benjamin Toornstra <benjamin.toornstra@vanoord.com>
2018-08-25 13:29:11 -0700

    doc: add Document.bookmarks

--- docx/document.py
+++ docx/document.py
@@ -5,10 +5,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from docx.blkcntnr import BlockItemContainer
+from docx.bookmark import Bookmarks
 from docx.enum.section import WD_SECTION
 from docx.enum.text import WD_BREAK
 from docx.section import Section, Sections
-from docx.shared import ElementProxy, Emu
+from docx.shared import ElementProxy, Emu, lazyproperty
 
 
 class Document(ElementProxy):
@@ -18,7 +19,7 @@ class Document(ElementProxy):
     a document.
     """
 
-    __slots__ = ("_part", "__body")
+    __slots__ = ("__body", "_bookmarks", "_part")
 
     def __init__(self, element, part):
         super(Document, self).__init__(element)
@@ -92,6 +93,16 @@ class Document(ElementProxy):
         table = self._body.add_table(rows, cols, self._block_width)
         table.style = style
         return table
+
+    @lazyproperty
+    def bookmarks(self):
+        """|Bookmarks| object providing access to |Bookmark| objects.
+
+        A bookmark may exist in the main document story, but also in headers,
+        footers, footnotes or endnotes. This collection contains all
+        bookmarks defined in any of these parts.
+        """
+        return Bookmarks(self._part)
 
     @property
     def core_properties(self):
