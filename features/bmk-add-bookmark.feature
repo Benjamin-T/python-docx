22141697fb425656032e3c9f5c9f3e2781b0a572
Benjamin Toornstra <benjamin.toornstra@vanoord.com>
2019-01-07 17:33:07 +0100

    acpt: Add bookmark to different story elements

---
+++ features/bmk-add-bookmark.feature
@@ -0,0 +1,19 @@
+Feature: Modifying bookmarks in various story parts
+  In order to add, remove or modify bookmarks in different story parts
+  As a developer using python-docx
+  I need bookmark start and end functionality in different story-elements
+
+  @wip
+  Scenario Outline: Add bookmark to different story elements
+    Given a <element>
+     When I start a bookmark named test in <element> as bookmark
+      And I terminate bookmark in <element>
+     Then len(bookmarks) == 1
+      And bookmarks[0] has name test
+
+  Examples: Different elements in story
+    | element   |
+    | document  |
+    | paragraph |
+    | header    |
+    | footer    |
--- features/steps/bookmarks.py
+++ features/steps/bookmarks.py
@@ -4,7 +4,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from behave import given, then
+from behave import given, then, when
 
 from docx import Document
 
@@ -20,7 +20,42 @@ def given_a_Bookmarks_object_of_length_5_as_bookmarks(context):
     context.bookmarks = document.bookmarks
 
 
+@given("a header")
+def given_a_header(context):
+    context.document = Document()
+    section = context.document.sections[0]
+    context.header = section.header
+
+
+@given("a footer")
+def given_a_footer(context):
+    context.document = Document()
+    section = context.document.sections[0]
+    context.footer = section.footer
+
+
+# when =====================================================
+
+
+@when("I start a bookmark named {name} in {element} as bookmark")
+def when_I_start_a_bookmark_named_test_in_document(context, name, element):
+    context.bookmarks = context.document.bookmarks
+    element = context.__getattr__(element)
+    context.bookmark = element.start_bookmark(name=name)
+
+
+@when("I terminate bookmark in {element}")
+def when_I_terminate_bookmark_in_story(context, element):
+    context.__getattr__(element).end_bookmark(context.bookmark)
+
+
 # then =====================================================
+
+
+@then("bookmarks[{idx}] has name {name}")
+def then_bookmark_has_name_test(context, idx, name):
+    bookmark = context.bookmarks[int(idx)]
+    assert bookmark.name == name
 
 
 @then("bookmarks.get({name}) returns _Bookmark named {name} and id {id}")
@@ -43,3 +78,10 @@ def then_iterating_bookmarks_produces_n_Bookmark_objects(context, n):
     items = [item for item in context.bookmarks]
     assert len(items) == int(n)
     assert all(item.__class__.__name__ == "_Bookmark" for item in items)
+
+
+@then("len(bookmarks) == {count}")
+def then_len_bookmarks_eq_count(context, count):
+    expected = int(count)
+    actual = len(context.bookmarks)
+    assert actual == expected, "len(bookmarks) == %s" % actual
